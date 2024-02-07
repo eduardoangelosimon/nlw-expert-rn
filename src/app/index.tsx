a5230999e8f66bcbfd1ffee759bf752fc2a8796c
Eduardo Angelo Simon <eduardosimon.trabalho@gmail.com>
2024-02-07 19:35:36 -0300

    aula 3

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
 import { View, Text, FlatList, SectionList } from "react-native";
-import { CATEGORIES, MENU } from "@/utils/data/products";
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products";
 import { useState, useRef } from "react";
 import { Product } from "@/components/product";
 import { Link } from "expo-router";
@@ -10,7 +10,7 @@ import { useCartStore } from "@/stores/cart-store";
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0]);
   const cartStore = useCartStore();
-  const sectionListRef = useRef<SectionList>(null);
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
 
   const cartQuantityItems = cartStore.products.reduce((total, product) => total + product.quantity, 0);
 
--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,8 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
 import * as cartInMemory from "./helpers/cart-in-memory";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 export type ProductCardProps = ProductProps & {
   quantity: number;
@@ -9,13 +11,30 @@ export type ProductCardProps = ProductProps & {
 type StateProps = {
   products: ProductCardProps[];
   add: (product: ProductProps) => void;
+  remove: (productId: string) => void;
+  clear: () => void;
 };
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
 
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
-}));
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
+
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+
+      clear: () => set(() => ({ products: [] })),
+    }),
+    {
+      name: "nlw-expert:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
